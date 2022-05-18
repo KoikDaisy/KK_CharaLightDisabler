3d5bdc73fd9149cbb427f68ce6d434616d16cc85
BuildTools <unconfigured@null.spigotmc.org>
2022-05-18 18:52:18 -0400

    add WaitForNull to studio map logic

--- src/PluginCode/KK_CharaLightDisabler.cs
+++ src/PluginCode/KK_CharaLightDisabler.cs
@@ -21,7 +21,7 @@ namespace KK_CharaLightDisabler
 
         public const string GUID = "koikdaisy.kkcharalightdisabler";
 
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         internal static new ManualLogSource Logger;
 
